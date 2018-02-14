45b748814f91690c010d481e51d83a48215473ad
bbd <zxczv41@gmail.com>
2018-02-14 10:14:09 +0900

    Add files via upload

---
+++ VariableExample.java
@@ -0,0 +1,8 @@
+public class VariableExample {
+    public static void main(String[] args) {
+        int value = 9090876;
+        int result = value + 1768978;
+
+        System.out.println(result);
+    }
+}
