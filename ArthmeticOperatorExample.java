70a4c783e1ea382ff3f6175ef63f5f26523df026
bbd <zxczv41@gmail.com>
2018-02-17 17:17:34 +0900

    Add files via upload

---
+++ ArthmeticOperatorExample.java
@@ -0,0 +1,17 @@
+public class ArthmeticOperatorExample {
+    public static void main(String[] args) {
+        int v1 = 5;
+        int v2 = 2;
+
+        int result1 = v1 + v2;
+        System.out.println("result = " + result1);
+        // Suggest. Double type
+
+        double v3 = v2;
+
+        double result2 = v1 / v3;
+
+        System.out.println("result = " + result2);
+        //c와 같이 1.0을 곱하거나 변수 중 하나를 double 타입으로 캐스팅 한 뒤 산술 연산을 하면 된다.
+    }
+}
