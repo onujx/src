e8fcebe7a03d1231e3b4de69a48423590f36b6bc
bbd <zxczv41@gmail.com>
2018-03-14 22:51:10 +0900

    Add files via upload

---
+++ DoYouKnow.java
@@ -0,0 +1,12 @@
+public class DoYouKnow {
+    public static void main(String[] args) {
+        int numberOne[] = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            numberOne[i] = i+1;
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(numberOne[i]);
+        }
+    }
+}
