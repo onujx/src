98101ec17820de853bf58f18f3153d9b7a5bed6b
bbd <zxczv41@gmail.com>
2018-07-10 21:05:39 +0900

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,7 +1,4 @@
 public class Car {
-    String company = "BMW";
-    String model = "i8";
-    String color = "Blue";
-    int maxSpeed = 350;
-    int speed;
+    class Tire {}
+    static class Engine{}
 }
