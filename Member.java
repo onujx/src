5caf26a30479f2f56f00de88d1313372c98a577c
bbd <zxczv41@gmail.com>
2019-12-30 10:44:30 +0900

    Add files via upload

--- Member.java
+++ Member.java
@@ -1,14 +1,6 @@
 public class Member {
-    boolean login(String id, String password) {
-        if (id.equals("hong") && password.equals("12345")) {
-
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    void logout(String id) {
-        System.out.println("로그아웃 되었습니다.");
-        }
-    }
+    String name;
+    String id;
+    String password;
+    int age;
+}
