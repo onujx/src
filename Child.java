330bea212caae6840c596fdf5f3244c8472f2bbb
bbd <zxczv41@gmail.com>
2020-01-09 09:12:00 +0900

    Add files via upload

--- Child.java
+++ Child.java
@@ -1,9 +1,4 @@
-package sec07.exam06_casting;
+package sec07.exam07_instanceof;
 
 public class Child extends Parent {
-	public String field2;
-	
-	public void method3() {
-		System.out.println("Child-method3()");
-	}
 }
